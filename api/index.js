fbf83e32c613a0849695ef78e331ef54dfeab388
Tyler Chase <tyler.chase.usa@gmail.com>
2016-09-13 13:43:23 -0600

    messages can be deleted

--- api/index.js
+++ api/index.js
@@ -5,8 +5,7 @@ let router = express.Router();
 const pg = require('../db/knex_config.js')
 
 router.post('/v1/items',(req, res, next) => {
-  console.log(pg)
-
+  //console.log(pg)
   pg('users').insert(req.body)
   .then(() =>{
     res.redirect('/')
@@ -26,5 +25,19 @@ router.post('/v1/items',(req, res, next) => {
     console.log('there was an error')
     next(err)
   })
+});
+
+router.get('/v1/items/delete/:id', (req, res, next) => {
+  // console.log("the id is: ", req.params.id);
+  // res.json(req.params)
+  pg('users').where('id', req.params.id).del()
+  .then((something)=>{
+  console.log(something);
+  res.redirect('/')
+  })
+  .catch((err) => {
+  console.error("error deleting from db");
+  next(err);
+  });
 });
 module.exports = router
